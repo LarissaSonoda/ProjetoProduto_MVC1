50cbbce2c1f34e811fb07a6b5d0a48acd0296c15
LarissaSonoda <larissa.sonoda@etec.sp.gov.br>
2021-06-14 16:47:05 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections.ObjectModel;
+using ProjetoPoduto.Models;
+using System.Web.Mvc;
+
+namespace ProjetoPoduto.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var produto = new Produto();
+            return View(produto);
+        }
+        [HttpPost]
+        public ActionResult Index(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Info", produto);
+            }
+            return View(produto);
+        }
+        public ActionResult Info(Produto produto)
+        {
+            return View(produto);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoPoduto.Models
+{
+    public class Produto
+    {
+        [Required(ErrorMessage ="Campo obrigatório")]
+        public int codigo { get; set; }
+
+        [Required()]
+        public string nome { get; set; }
+
+        [StringLength(200, MinimumLength = 10, ErrorMessage ="Insira uma descrição de 10 a 200 caracteres")]
+        public string desc { get; set; }
+
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Valor inválido")]
+        public string peso { get; set; }
+
+        [Required(ErrorMessage ="Insira a categoria do produto")]
+        public string categoria { get; set; }
+
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage="Valor inválido")]
+        public string valUnit { get; set;  }
+    }
+}
